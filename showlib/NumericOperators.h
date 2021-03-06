a86b1a5b7f07db04dbb3a648d27a579bc578fec9
Joe Larson <jpl@showpage.org>
2021-03-06 12:21:49 -0600

    Added another version of inRange.

--- showlib/NumericOperators.h
+++ showlib/NumericOperators.h
@@ -3,5 +3,12 @@
 #include <string>
 
 namespace ShowLib {
-    inline bool inSizeRange(int value, size_t maxValue) { return value >= 0 && static_cast<size_t>(value) < maxValue; }
+    inline bool inSizeRange(int value, size_t maxValue) {
+        return value >= 0 && static_cast<size_t>(value) < maxValue;
+    }
+
+    template <class T>
+    inline bool inSizeRange(int value, std::vector<T> vec) {
+        return value >= 0 && static_cast<size_t>(value) < vec.size();
+    }
 }
