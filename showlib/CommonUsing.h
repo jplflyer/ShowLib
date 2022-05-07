69791e78130bedbef5a6e7967b0e3813cc54e126
Joseph Larson <joseph.larson@vitac.com>
2022-05-07 12:17:04 -0500

    Copied this from work.

---
+++ showlib/CommonUsing.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+
+//======================================================================
+// The most common using statements I do in most of my code.
+//======================================================================
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+
+using namespace std::chrono_literals;
