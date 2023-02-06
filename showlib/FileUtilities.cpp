99b35537f8af49aeed22f1ba47c272d5ac789d40
Joseph Larson <jpl@showpage.org>
2023-02-05 18:47:40 -0600

    Added readLines method.

--- showlib/FileUtilities.cpp
+++ showlib/FileUtilities.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 
 #include "FileUtilities.h"
+#include "StringUtils.h"
 
 using std::string;
 
@@ -20,4 +21,22 @@ FileUtilities::readFile(const string &filename)
     }
 
 	return string( (std::istreambuf_iterator<char>(ifs) ), (std::istreambuf_iterator<char>() ) );
+}
+
+/**
+ * Read lines from the file, returning one line per entry in the vector.
+ */
+StringVector readLines(const std::string &filename, bool doTrim) {
+    StringVector vec;
+    std::ifstream ifs(filename);
+    string line;
+
+    while (getline(ifs, line)) {
+        if (doTrim) {
+            trimInPlace(line);
+        }
+        vec.add(line);
+    }
+
+    return vec;
 }
--- showlib/FileUtilities.h
+++ showlib/FileUtilities.h
@@ -2,8 +2,11 @@
 
 #include <string>
 
+#include "StringVector.h"
+
 namespace ShowLib {
     namespace FileUtilities {
         std::string readFile(const std::string &filename);
+        StringVector readLines(const std::string &filename, bool doTrim = false);
     }
 }
